2fcae62251ed2600874fbf1c64f8983a0ea3fbe4
Moritz Fromm <git@moritz-fromm.de>
2019-04-17 00:38:48 +0200

    Change default username from `blau` to `Dinnerbone`

--- options/options.js
+++ options/options.js
@@ -8,7 +8,7 @@ function saveOptions(e) {
   function restoreOptions() {
   
     function setCurrentChoice(result) {
-      document.querySelector("#username").value = result.username || "blue";
+      document.querySelector("#username").value = result.username || "Dinnerbone";
     }
   
     function onError(error) {
