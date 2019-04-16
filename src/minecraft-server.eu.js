e0686c71774adea0ce9b6eca40490b2ade61c1bc
Moritz Fromm <git@moritz-fromm.de>
2019-04-16 12:49:12 +0200

    Add settings module

--- src/minecraft-server.eu.js
+++ src/minecraft-server.eu.js
@@ -1,1 +1,10 @@
-document.getElementById('playername').value = 'dinnerbone';
+function onError(error) {
+  console.warn(error);
+}
+
+function onGot(settings) {
+  document.getElementById("playername").value = settings.username;
+}
+
+var getting = browser.storage.sync.get("username");
+getting.then(onGot, onError);
--- src/minecraft-serverlist.net.js
+++ src/minecraft-serverlist.net.js
@@ -1,1 +1,10 @@
-document.getElementById('mcname').value = 'dinnerbone';
+function onError(error) {
+  console.warn(error);
+}
+
+function onGot(settings) {
+    document.getElementById("mcname").value = settings.username;
+}
+
+var getting = browser.storage.sync.get("username");
+getting.then(onGot, onError);
