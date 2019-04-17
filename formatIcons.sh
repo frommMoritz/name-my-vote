2410407ccfc2a9e05b6691fe25bfe87753a013c6
Moritz Fromm <git@moritz-fromm.de>
2019-04-17 11:25:57 +0200

    Add 32x and 16x icon

--- formatIcons.sh
+++ formatIcons.sh
@@ -6,3 +6,5 @@ convert icon-max.png -resize 512 icon-512.png
 convert icon-max.png -resize 256 icon-256.png
 convert icon-max.png -resize 128 icon-128.png
 convert icon-max.png -resize 64 icon-64.png
+convert icon-max.png -resize 32 icon-32.png
+convert icon-max.png -resize 16 icon-16.png
